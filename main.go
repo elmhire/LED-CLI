77aa4763e36a315d0d2e0577c0f47d0149ed07ee
Peter Wirtz <pwirtz13@gmail.com>
2022-03-31 19:14:13 -0500

    Finished implementing CSV Functionality

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/base64"
+	"encoding/csv"
 	"errors"
 	"fmt"
 	"io"
@@ -37,14 +38,15 @@ func main() {
 	filename := getFileName()
 
 	// Get data from file
-	fmt.Printf("Opening %s...\n\n", filename)
+	fmt.Printf("Opening %s...", filename)
 	data := getRawFileDataAsStr(filename)
 	content := convertToUTF8(data)
+	fmt.Printf("Success!\n\n")
 
 	// Extract links from contents
 	links, err := extractLinks(content)
 	if err != nil {
-		fmt.Println("No files found to download.")
+		fmt.Println("No files found to download. Exiting.")
 		normalExit()
 	}
 
@@ -57,7 +59,34 @@ func main() {
 	fmt.Printf("%d of %d files downloaded completely.\n\n", complete, numLinks)
 
 	// Gather data for future CSV output
-	csvData := getDataFromFiles(numLinks)
+	fmt.Printf("Parsing downloaded data... ")
+	csvData := getDataFromFiles()
+
+	csvText := [][]string{
+		{"location", "invoice_number", "total"},
+	}
+
+	for _, e := range csvData {
+		csvText = append(csvText, []string{e.location, e.invoice_number, e.total})
+	}
+
+	fmt.Printf("Success!\n\n")
+
+	// Write csv file
+	fmt.Printf("Writing data to 'downloaded_invoice_data.csv'... ")
+	f, err := os.Create("downloaded_invoice_data.csv")
+	defer f.Close()
+	if err != nil {
+		log.Fatalln("Failed to create file", err)
+	}
+
+	w := csv.NewWriter(f)
+	err = w.WriteAll(csvText)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Success!\n\n")
 
 	// Rename files
 	fmt.Println("Renaming files...")
@@ -302,8 +331,7 @@ type CSVEntry struct {
 	originalFileName string
 	newFileName      string
 	location         string
-	locationEscaped  string
-	invoice          string
+	invoice_number   string
 	total            string
 }
 
@@ -314,14 +342,15 @@ func newEntry(fileName string) *CSVEntry {
 	if err != nil {
 		panic(err)
 	}
-	e.location, e.locationEscaped = getShipToName(invoice)
-	e.newFileName = strings.Join([]string{e.locationEscaped, e.originalFileName}, "_")
-	e.total = "0.00"
+	e.location = getShipToName(invoice)
+	e.invoice_number = strings.TrimRight(fileName, ".pdf")
+	e.newFileName = strings.Join([]string{escapeString(e.location), e.originalFileName}, "_")
+	e.total = getTotal(invoice)
 
 	return &e
 }
 
-func getDataFromFiles(total int) []CSVEntry {
+func getDataFromFiles() []CSVEntry {
 	pdf.DebugOn = true
 	files := getPdfFiles()
 	var csvEntries []CSVEntry
@@ -330,17 +359,6 @@ func getDataFromFiles(total int) []CSVEntry {
 		csvEntries = append(csvEntries, *newEntry(file))
 	}
 	return csvEntries
-}
-
-func renameFile(index int, total int, entry CSVEntry) {
-	os.Rename(entry.originalFileName, entry.newFileName)
-	fmt.Printf(
-		"Renaming %d of %d: %s to %s\n",
-		(index + 1),
-		total,
-		entry.originalFileName,
-		entry.newFileName,
-	)
 }
 
 func getPdfFiles() (pdfList []string) {
@@ -373,11 +391,9 @@ func readPdf(path string) (string, error) {
 	return buf.String(), nil
 }
 
-func getShipToName(pdfStr string) (string, string) {
-	var re = regexp.MustCompile(`('|,)`)
+func getShipToName(pdfStr string) string {
 	var inParens = false
 	pdfStr = pdfStr[strings.Index(pdfStr, "SHIP TO:")+8:]
-	var pdfStrEscaped string
 
 	// Loop through pdfStr one character at a time (RUNE)
 	// until curChar is a number digit after a close paren
@@ -395,12 +411,36 @@ func getShipToName(pdfStr string) (string, string) {
 		}
 		if unicode.IsDigit(curChar) {
 			pdfStr = pdfStr[:i]
-			pdfStrEscaped = strings.Replace(pdfStr, " ", "_", -1)
-			pdfStrEscaped = re.ReplaceAllString(pdfStrEscaped, ``)
 			break
 		}
 	}
-	return pdfStr, pdfStrEscaped
+	return strings.TrimSpace(pdfStr)
+}
+
+func getTotal(pdfStr string) string {
+	var pleaseLoc = strings.Index(pdfStr, "Please")
+	return strings.TrimSpace(
+		strings.Trim(
+			pdfStr[pleaseLoc-8:pleaseLoc], "$"),
+	)
+}
+
+func escapeString(str string) string {
+	var re = regexp.MustCompile(`('|,)`)
+	str = strings.Replace(str, " ", "_", -1)
+	str = re.ReplaceAllString(str, ``)
+	return str
+}
+
+func renameFile(index int, total int, entry CSVEntry) {
+	os.Rename(entry.originalFileName, entry.newFileName)
+	fmt.Printf(
+		"Renaming %d of %d: %s to %s\n",
+		(index + 1),
+		total,
+		entry.originalFileName,
+		entry.newFileName,
+	)
 }
 
 func normalExit() {
