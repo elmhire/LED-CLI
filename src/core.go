757970c5308f6b2640944a29535ea48a707ae4c8
Peter Wirtz <pwirtz13@gmail.com>
2022-04-08 10:58:58 -0500

    More restructuring

--- src/core.go
+++ src/core.go
@@ -9,14 +9,10 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
-	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
-	"unicode"
 
 	"github.com/DusanKasan/parsemail"
 	"github.com/ledongthuc/pdf"
@@ -35,8 +31,8 @@ func check(err error) {
 func GetFileName() (choice string) {
 	files := GetCwdEmailList()
 	for {
-		fmt.Println("Please select the email to open.")
-		fmt.Println()
+		fmt.Printf("Please select the email to open.\n\n")
+
 		for i, file := range files {
 			fmt.Printf("(%d) %s\n", i+1, file)
 		}
@@ -167,12 +163,6 @@ func ConvertToUTF8(dat string) (s string) {
 	return
 }
 
-// Link ...
-type Link struct {
-	url  string
-	text string
-}
-
 // extractLinks
 func ExtractLinks(htmlText string) (links []Link, e error) {
 	node, err := html.Parse(strings.NewReader(htmlText))
@@ -187,11 +177,7 @@ func ExtractLinks(htmlText string) (links []Link, e error) {
 				if a.Key == "href" &&
 					!strings.Contains(a.Val, "mailto") &&
 					!strings.Contains(n.FirstChild.Data, "here") {
-					link := Link{
-						url:  a.Val,
-						text: n.FirstChild.Data,
-					}
-					links = append(links, link)
+					links = append(links, *newLink(a.Val, n.FirstChild.Data))
 					break
 				} // End if
 			} // End For
@@ -211,8 +197,7 @@ func ExtractLinks(htmlText string) (links []Link, e error) {
 func DownloadLinks(links []Link) (complete int) {
 	for i, link := range links {
 		fmt.Printf("Downloading %s %d of %d...", link.text, i+1, len(links))
-		err := downloadFile(link, true)
-		if err != nil {
+		if err := link.download(true); err != nil {
 			fmt.Printf("\terror, incomplete!\n")
 			continue
 		}
@@ -222,160 +207,26 @@ func DownloadLinks(links []Link) (complete int) {
 	return
 }
 
-func downloadFile(link Link, useLinkName bool, pathOptional ...string) (err error) {
-	var (
-		path     string
-		filename string
-	)
-
-	if len(pathOptional) > 0 {
-		path = pathOptional[0]
-	} else {
-		path = "."
-	}
-
-	response, err := http.Get(link.url)
-	check(err)
-
-	defer response.Body.Close()
-
-	// Decide whether to use link.text as filename
-	// or the name that the server gives us
-	// Right now we only use link.text as filename
-	if useLinkName {
-		filename = fmt.Sprintf("%s/%s", path, link.text)
-	} else {
-		filename = filepath.Base(response.Request.URL.EscapedPath())
-		temp, err := url.PathUnescape(filename)
-		if err == nil {
-			filename = temp
-		}
-	}
-
-	// Create the file
-	out, err := os.Create(fmt.Sprintf("%s/%s", path, filename))
-	if err != nil {
-		return err
-	}
-
-	defer out.Close()
-
-	// Write the body to file
-	_, err = io.Copy(out, response.Body)
-	out.Close()
-
-	return
-}
-
-type CsvEntry struct {
-	originalFileName string
-	newFileName      string
-	location         string
-	invoice_number   string
-	total            string
-}
-
-func (e CsvEntry) GetLocation() string {
-	return e.location
-}
-
-func (e CsvEntry) GetInvoiceNum() string {
-	return e.invoice_number
-}
-
-func (e CsvEntry) GetTotal() string {
-	return e.total
-}
-
-func newEntry(fileName string) (e *CsvEntry) {
-	e = &CsvEntry{originalFileName: fileName}
-
-	invoice, err := readPdf(fileName) // Read local pdf file
-	if err != nil {
-		panic(err)
-	}
-	e.location = getShipToName(invoice)
-	e.invoice_number = strings.TrimRight(fileName, ".pdf")
-	e.newFileName = strings.Join([]string{escapeString(e.location), e.originalFileName}, "_")
-	e.total = getTotal(invoice)
-
-	return
-}
-
-func GetDataFromFiles() (csvEntries []CsvEntry) {
+func GetDataFromFiles() (csvEntries []InvoiceEntry) {
 	pdf.DebugOn = true
-	files := getPdfFiles()
+	var pdfList []string
 
-	for _, file := range files {
-		csvEntries = append(csvEntries, *newEntry(file))
-	}
-	return
-}
-
-func getPdfFiles() (pdfList []string) {
 	files, err := ioutil.ReadDir(".")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for _, file := range files {
-		name := file.Name()
-		if strings.HasPrefix(name, "SI-") && strings.Contains(name, ".pdf") {
-			pdfList = append(pdfList, name)
+		if fName := file.Name(); strings.HasPrefix(fName, "SI-") &&
+			strings.HasSuffix(fName, ".pdf") {
+			pdfList = append(pdfList, fName)
 		}
+	}
+
+	for _, pdfName := range pdfList {
+		csvEntries = append(csvEntries, *newEntry(pdfName))
 	}
 	return
-}
-
-func readPdf(path string) (bs string, err error) {
-	f, r, err := pdf.Open(path)
-	// remember close file
-	defer f.Close()
-	if err != nil {
-		return "", err
-	}
-	var buf bytes.Buffer
-	b, err := r.GetPlainText()
-	if err != nil {
-		bs = ""
-		return
-	}
-	buf.ReadFrom(b)
-	bs = buf.String()
-	return
-}
-
-func getShipToName(pdfStr string) (s string) {
-	var inParens = false
-	pdfStr = pdfStr[strings.Index(pdfStr, "SHIP TO:")+8:]
-
-	// Loop through pdfStr one character at a time (RUNE)
-	// until curChar is a number digit after a close paren
-	// that marks the end of the "Ship To" name.
-	for i, curChar := range pdfStr {
-		if string(curChar) == "(" {
-			inParens = true
-		}
-		if inParens {
-			if string(curChar) == ")" {
-				inParens = false
-			} else {
-				continue
-			}
-		}
-		if unicode.IsDigit(curChar) {
-			s = strings.TrimSpace(pdfStr[:i])
-			break
-		}
-	}
-	return
-}
-
-func getTotal(pdfStr string) string {
-	var pleaseLoc = strings.Index(pdfStr, "Please")
-	return strings.TrimSpace(
-		strings.Trim(
-			pdfStr[pleaseLoc-8:pleaseLoc], "$"),
-	)
 }
 
 func escapeString(str string) string {
@@ -386,14 +237,15 @@ func escapeString(str string) string {
 	)
 }
 
-func RenameFile(index int, total int, entry CsvEntry) {
-	os.Rename(entry.originalFileName, entry.newFileName)
+func RenameFile(index int, total int, entry InvoiceEntry) {
+	origName, newName := entry.GetFileNames()
+	os.Rename(origName, newName)
 	fmt.Printf(
 		"Renaming %d of %d: %s to %s\n",
 		(index + 1),
 		total,
-		entry.originalFileName,
-		entry.newFileName,
+		origName,
+		newName,
 	)
 }
 
